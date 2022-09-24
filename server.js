7556a28b518dc99b169ca171b36837664493bd9b
Aaronr99 <38481944+Aaronr99@users.noreply.github.com>
2022-09-24 03:32:55 -0300

    fix: mongo atlas connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 //process.env.DATABASE_URL (local)
-mongoose.connect('mongodb://' + process.env.APP_NAME)
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
