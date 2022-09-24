e62cf12ee24f7562b452fc94ee04447497a3f81f
Aaronr99 <38481944+Aaronr99@users.noreply.github.com>
2022-09-24 04:02:33 -0300

    removed cors

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const cors = require('cors')
+//const cors = require('cors')
 
-const corsOptions = {
+/*const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
-app.use(cors(corsOptions));
+app.use(cors(corsOptions));*/
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
